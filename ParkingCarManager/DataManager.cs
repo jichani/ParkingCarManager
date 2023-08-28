7d947b716ae50065b5383e3f9f6326f2436f071a
jichani <jichan7571@naver.com>
2023-08-28 19:34:43 +0900

    초기 커밋.

---
+++ ParkingCarManager/DataManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingCarManager
+{
+    public class DataManager
+    {
+
+        // ParkingCarManager.exe랑 같은 경로에 LogFolder라는 이름의 폴더가 없다면..
+        public static void printLog (string contents)
+        {
+            DirectoryInfo di = new DirectoryInfo ("LogFolder");
+            if (di.Exists == false) 
+            {
+                // 새로 만든다.
+                di.Create();
+            }
+            // @"LogFolder\ParkingHistory.txt"
+            // = "LogFolder\\ParkingHistory.txt"
+            // true는 append 옵션을 true. 새로운 텍스트가 나오면 덮어쓰지 않고 뒤에다가 새로 이어붙인다.
+            using (StreamWriter w = new StreamWriter(@"LogFolder\ParkingHistory.txt",true))
+            {
+                w.WriteLine(contents);
+            } ;
+        }
+    }
+}
---
+++ ParkingCarManager/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParkingCarManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = DateTime.Now.ToString("yyyy년 MM월 dd일 HH시 mm분 ss초");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("yyyy년 MM월 dd일 HH시 mm분 ss초");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WriteLog("버튼1 클릭");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void WriteLog(string contents)
+        {
+            string log = $"[{DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss.fff")}]";
+            log += contents;
+            DataManager.printLog(log);
+            listBox1.Items.Insert(0, log); // 새로운 내용이 맨 위로 올라온다.
+            // listBox1.Items.Add(0, log); // 새로운 내용이 맨 밑으로 간다.
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WriteLog("버튼2 클릭");
+        }
+    }
+    
+}
